38a27ccebaf0cb4cb5726e3f97b8994c0af5508f
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-06-25 12:07:51 +0200

    Time: 1559 ms (9.03%) | Memory: 23.5 MB (10.50%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -3,13 +3,19 @@ class Solution:
         result=set()
         nums.sort()
         for i in range (0,len(nums)):
-            if  nums[i]<=0:
-                target=-1*(nums[i])
-                hashset={}
-                for n in range(i+1,len(nums)):
-                    diff=target-nums[n]
-                    if diff in hashset:
-                        result.add(tuple([nums[i],diff,nums[n]]))
-                    hashset[nums[n]]=n
+            if  nums[i]>0:
+                break
+            target=-1*(nums[i])
+            left=i+1
+            right=len(nums)-1
+            while(left<right):
+                if (nums[left]+nums[right])==target:
+                    result.add((nums[i],nums[left],nums[right]))
+                    left+=1
+                    right-=1
+                if (nums[left]+nums[right])>target:
+                    right-=1
+                elif (nums[left]+nums[right])<target:
+                    left+=1
         return [list(t) for t in result]
 
