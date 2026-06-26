b66672c2417e75f8bebab9a7286fcad621f0efb6
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-06-26 13:35:59 +0200

    Time: 0 ms (100.00%) | Memory: 19.3 MB (73.23%) - LeetSync

---
+++ 990-verifying-an-alien-dictionary/verifying-an-alien-dictionary.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAlienSorted(self, words: List[str], order: str) -> bool:
+        hashmap={}
+        for i in range(len(order)):
+            hashmap[order[i]]=i
+        for i in range(len(words)-1):
+            for j in range(len(words[i])):
+                if j>=len(words[i+1]):
+                    return False
+                if words[i][j]!=words[i+1][j]:
+                    if hashmap[words[i+1][j]]<hashmap[words[i][j]]:
+                        return False
+                    else:
+                        break
+        return True
