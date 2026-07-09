270e1d2f854d9dbfeec3d2aac98372263587592e
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-07-09 11:15:07 +0200

    Time: 6 ms (45.25%) | Memory: 20.5 MB (61.20%) - LeetSync

--- 283-move-zeroes/move-zeroes.py
+++ 283-move-zeroes/move-zeroes.py
@@ -3,12 +3,10 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        insert_pos = 0
-
+        index=0
         for i in range(len(nums)):
-            if nums[i] != 0:
-                # Only swap when the element isn't already in position
-                if i != insert_pos:
-                    nums[insert_pos] = nums[i]
-                    nums[i] = 0
-                insert_pos += 1
+            if nums[i] !=0:
+                nums[index]=nums[i]
+                index+=1
+        for i in range(index,len(nums)):
+            nums[i]=0
