fabc2361df649d86b6b1f299570cb7a1059c65cc
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-07-31 17:22:48 +0200

    Time: 0 ms (100.00%) | Memory: 43.6 MB (94.00%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cs
@@ -1,15 +1,10 @@
 public class Solution {
     public int MaxProfit(int[] prices) {
-        int ptr1=prices.Length-1;
-        int ptr2=prices.Length-2;
         int sum=0;
-        while(ptr1>0){
-        if(prices[ptr1]>prices[ptr2])
-        {
-            sum+=(prices[ptr1]-prices[ptr2]);
-        }
-        ptr1-=1;
-        ptr2-=1;
+        for(int i=1;i<prices.Length;i++){
+            if(prices[i]>prices[i-1]){
+                sum+=(prices[i]-prices[i-1]);
+            }
         }
         return sum;
     }
