96a495a6d6c349a5e59085f3a1d21c81857f22bc
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-08-03 14:54:53 +0530

    Delete 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql

--- 1724-customer-who-visited-but-did-not-make-any-transactions/customer-who-visited-but-did-not-make-any-transactions.sql
+++
@@ -1,2 +0,0 @@
-/* Write your T-SQL query statement below */
-select customer_id , count(customer_id) as count_no_trans from visits as v left join transactions as t on v.visit_id=t.visit_id where transaction_id is null group by customer_id
