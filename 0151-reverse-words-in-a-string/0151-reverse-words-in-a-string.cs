89f2668b0531932f5afe830faaa295ba7e3da06f
blessyjustin <128029911+blessyjustin@users.noreply.github.com>
2026-08-29 16:04:03 +0530

    Time: 10 ms (24.47%), Space: 56.5 MB (20.37%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cs
@@ -1,12 +1,10 @@
 public class Solution {
     public string ReverseWords(string s) {
-        s=s.Trim();
-        string[]  st=s.Split(' ');
+        string[]  st=s.Split(' ',StringSplitOptions.RemoveEmptyEntries);
         string ans="";
-        for(int i=st.Length-1;i>0;i--){
-            if(st[i].Length>0){
-            ans+=st[i]+" ";}
+        for(int i=st.Length-1;i>=0;i--){
+            ans+=st[i]+" ";
         }
-        return ans+st[0];
+        return ans.Trim();
     }
 }
